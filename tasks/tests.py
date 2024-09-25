8ecb6ad1126fef4f512899cb296b7a817cd1ed27
DSahalatyi <dsahalatyi@gmail.com>
2024-09-25 14:00:25 +0300

    Add tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from tasks.forms import TaskForm
+from tasks.models import Tag, Task
+
+
+class TagModelTests(TestCase):
+    def test_tag_str(self):
+        tag = Tag.objects.create(name="Test Tag")
+        self.assertEqual(str(tag), "Test Tag")
+
+
+class TaskFormTests(TestCase):
+    def test_task_form_valid(self):
+        form = TaskForm(
+            data={
+                "content": "Test task",
+                "deadline": timezone.now() + timedelta(hours=1),
+            }
+        )
+
+    def test_task_form_invalid(self):
+        form = TaskForm(
+            data={
+                "content": "Test task",
+                "deadline": timezone.now() - timedelta(hours=1),
+            }
+        )
+
+
+class TaskViewsTests(TestCase):
+    def setUp(self):
+        self.task = Task.objects.create(
+            content="Test task",
+        )
+
+    def test_task_list_post_complete_task(self):
+        form_data = {
+            "action": "complete",
+            "task_id": self.task.id,
+        }
+        response = self.client.post(reverse("tasks:task-list"), data=form_data)
+        self.assertRedirects(response, reverse("tasks:task-list"))
+        self.task.refresh_from_db()
+        self.assertTrue(self.task.is_done)
