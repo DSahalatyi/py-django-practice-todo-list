eb1162160f644c07a474fe1634b613a986088a2b
DSahalatyi <dsahalatyi@gmail.com>
2024-09-25 11:36:15 +0300

    Add tags CRUD pages

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 
 from tasks.forms import TaskForm
-from tasks.models import Task
+from tasks.models import Task, Tag
 
 
 class TaskListView(ListView):
@@ -15,3 +15,25 @@ class TaskCreateView(CreateView):
     form_class = TaskForm
     template_name = "tasks/task_form.html"
     success_url = reverse_lazy("tasks:task-list")
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "tasks/tag_list.html"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = ("name",)
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = ("name",)
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tasks:tag-list")
